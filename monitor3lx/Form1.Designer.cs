1953f55be4c124dc9d2907a91c93da87d0bab63b
vvp63 <vvpoliektov@mail.ru>
2020-04-14 11:33:54 +0300

    Starting version

---
+++ monitor3lx/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace monitor3lx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.bConn = new System.Windows.Forms.Button();
+            this.bGetTP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbLog
+            // 
+            this.tbLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tbLog.Location = new System.Drawing.Point(0, 191);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.Size = new System.Drawing.Size(1021, 230);
+            this.tbLog.TabIndex = 0;
+            // 
+            // bConn
+            // 
+            this.bConn.Location = new System.Drawing.Point(13, 24);
+            this.bConn.Name = "bConn";
+            this.bConn.Size = new System.Drawing.Size(75, 23);
+            this.bConn.TabIndex = 1;
+            this.bConn.Text = "Connect";
+            this.bConn.UseVisualStyleBackColor = true;
+            this.bConn.Click += new System.EventHandler(this.ConnClick);
+            // 
+            // bGetTP
+            // 
+            this.bGetTP.Location = new System.Drawing.Point(336, 23);
+            this.bGetTP.Name = "bGetTP";
+            this.bGetTP.Size = new System.Drawing.Size(75, 23);
+            this.bGetTP.TabIndex = 2;
+            this.bGetTP.Text = "getTP";
+            this.bGetTP.UseVisualStyleBackColor = true;
+            this.bGetTP.Click += new System.EventHandler(this.getTP);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 421);
+            this.Controls.Add(this.bGetTP);
+            this.Controls.Add(this.bConn);
+            this.Controls.Add(this.tbLog);
+            this.Name = "Form1";
+            this.Text = "monitor3lx";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.Button bConn;
+        private System.Windows.Forms.Button bGetTP;
+    }
+}
+
---
+++ monitor3lx/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using Renci.SshNet;
+using System.IO;
+
+
+
+namespace monitor3lx
+{
+    public partial class Form1 : Form
+    {
+
+        SshClient gCl;
+        NpgsqlConnection gConn;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextLog(string aLogStr, params object[] aParams)
+        {
+            tbLog.AppendText(string.Format(aLogStr, aParams) + "\r\n");
+        }
+
+        private void ConnClick(object sender, EventArgs e)
+        {
+
+            string  host = "195.138.210.152";
+            string  username = "aquila";
+            string  password = "SQL16384";
+            string  keyFile = @"D:\Poliektov\tmp\sld\vv_dev\SshHostKeys\aquila_sec_ssh.ppk";
+            string  passPharse = "moonspell";
+            string  database = "MTS3pg";
+            int     pgPort = 5432;
+            int     fwdPort = 15432;
+            
+
+
+
+            ConnectionInfo ConnNfo = new ConnectionInfo(host, 22, username,
+                new AuthenticationMethod[]{
+                        // Key Based Authentication (using keys in OpenSSH Format)
+                        new PrivateKeyAuthenticationMethod(username, new PrivateKeyFile[]  {
+                                        new PrivateKeyFile(keyFile, passPharse)
+                                    }),
+                 }
+              );
+            
+            gCl = new SshClient(ConnNfo);
+            gCl.Connect();
+            TextLog("SSH connected = {0}",  gCl.IsConnected);
+
+            var port = new ForwardedPortLocal("127.0.0.1", (uint)fwdPort, "127.0.0.1", (uint)pgPort);
+            gCl.AddForwardedPort(port);
+            port.Start();
+
+            TextLog("Port Forwarding = {0}", port.IsStarted);
+
+           
+            string vConnStr = string.Format("Host=127.0.0.1; Port={0}; Database={1}; Username={2}; Password={3};",
+                                                fwdPort, database, username, password);
+            gConn = new NpgsqlConnection(vConnStr);
+            gConn.Open();
+            TextLog("Postgres state = {0}", gConn.State);
+
+
+
+
+            /*
+            string vConnStr = "Host=195.138.210.152; Port=22; Username=aquila; Integrated Security=true; Database=MTS3Pg" +
+                "Client Certificate=D:/distr/putty.org.ru/PuTTY PORTABLE/SshHostKeys/aquila_pub";
+            NpgsqlConnection gConn = new NpgsqlConnection(vConnStr);
+            gConn.Open();
+            tbLog.AppendText(gConn.State.ToString());
+            */
+
+        }
+
+        private void getTP(object sender, EventArgs e)
+        {
+            //
+            if (gConn.State == ConnectionState.Open)
+            {
+                TextLog("Connection ok");
+                using (var cmd = new NpgsqlCommand("SELECT * FROM public.securities", gConn))
+                using (var dr = cmd.ExecuteReader()) {
+                    DataTable vSchema = dr.GetSchemaTable();
+                    foreach (DataRow myField in vSchema.Rows)
+                    {
+                        foreach (DataColumn myProperty in vSchema.Columns)
+                            TextLog("{0} = {1}", myProperty.ColumnName, myField[myProperty]);
+                    }
+/*
+                    while (dr.Read()) {
+                        TextLog("---{0} {1} {2}", dr.FieldCount, dr[0], dr[1]);
+                    }
+                    */
+                }
+
+            }
+        }
+    }
+
+
+    
+
+}
