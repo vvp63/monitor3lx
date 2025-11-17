46846933717df0b55180c6f7b95b0ab605961fe0
vvp63 <vvpoliektov@mail.ru>
2025-11-17 14:33:39 +0300

    CloseBasisChange added

--- monitor3lx/Form1.cs
+++ monitor3lx/Form1.cs
@@ -592,7 +592,7 @@ namespace monitor3lx
 
         private void BC_Enter(object sender, EventArgs e)
         {
-            FillDGVByQuery(dgv_BC_Params, "SELECT \"TPId\", \"RepoPerc\", \"DivSumm\", \"Spread\" FROM \"public\".\"tp_basis_count\" WHERE \"IsActive\" = B'1' ORDER BY \"TPId\"");
+            FillDGVByQuery(dgv_BC_Params, "SELECT \"TPId\", \"RepoPerc\", \"DivSumm\", \"Spread\", \"CloseBasisChange\" FROM \"public\".\"tp_basis_count\" WHERE \"IsActive\" = B'1' ORDER BY \"TPId\"");
         }
 
         private void BC_EndEdit(object sender, DataGridViewCellEventArgs e)
